f23a130070f0441f454f7860885a86f6e5245253
fumizo07 <87250591+fumizo07@users.noreply.github.com>
2026-02-04 10:16:30 +0900

    Create eruda.user.js

---
+++ archive/eruda.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         eruda
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  Console for mobile browsers
+// @author       kairusds
+// @include      http://*
+// @include      https://*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/eruda/3.2.2/eruda.min.js
+// @icon         https://www.google.com/s2/favicons?domain=greasyfork.org
+// @downloadURL  https://gist.githubusercontent.com/kairusds/d98aaf7af7cfeed5ae4d91493c0c89b0/raw/eruda.js
+// @supportURL   https://gist.github.com/kairusds/d98aaf7af7cfeed5ae4d91493c0c89b0
+// @homepageURL  https://gist.github.com/kairusds/d98aaf7af7cfeed5ae4d91493c0c89b0
+// @run-at       document-body
+// @grant        none
+// ==/UserScript==
+
+(() => {
+	eruda.init();
+})();
